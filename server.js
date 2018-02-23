026696059e583ddeef569918d276e79e7217176b
Simas2006 <sjuknelis@gmail.com>
2018-02-23 17:46:08 -0500

    Add vote function + article file

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
+var fs = require("fs");
 var express = require("express");
 var app = express();
 var PORT = process.argv[2] || 8000;
+var LIMIT = 15 * 60 * 1000;
+var recentVotes = {};
+var articles;
 
-
+app.use("/vote",function(request,response) {
+  var url = request.url.split("?")[0];
+  var qs = request.url.split("?").slice(1).join("?").split(",");
+  var ip = request.connection.remoteAddress || request.headers["x-forwarded-for"];
+  if ( ! articles[qs[0]] ) {
+    console.log(`REJECT notfound ${ip} ${qs[0]}`);
+    response.writeHead(404);
+    response.write("err_no_article");
+    response.end();
+    return;
+  }
+  if ( ! recentVotes[ip] ) recentVotes[ip] = [];
+  var item = recentVotes[ip].filter(item => item.article == qs[0])[0];
+  if ( item ) {
+    if ( item.expires <= new Date().getTime() ) {
+      recentVotes[ip].splice(recentVotes[ip].indexOf(item),1);
+    } else {
+      console.log(`REJECT expire ${ip} ${qs[0]}`);
+      response.writeHead(400);
+      response.write("err_no_expire");
+      response.end();
+      return;
+    }
+  }
+  recentVotes[ip].push({
+    article: qs[0],
+    expires: new Date().getTime() + LIMIT
+  });
+  articles[qs[0]].votes[qs[1]][qs[2]]++;
+  console.log(`VOTE ${ip} ${qs[0]} ${qs[1]}`);
+  response.writeHead(200);
+  response.write("ok");
+  response.end();
+});
 
 app.listen(PORT,function() {
+  fs.readFile(__dirname + "/articles.json",function(err,data) {
+    if ( err ) throw err;
+    articles = JSON.parse(data.toString());
+  });
   console.log("Listening on port " + PORT);
 });
