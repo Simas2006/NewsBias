ba34c3f8514345928d952e34a619d577e5a0b413
Simas2006 <sjuknelis@gmail.com>
2018-03-06 16:50:32 -0500

    Add rendering function for home page

---
+++ web/home/script.js
@@ -0,0 +1,65 @@
+var searchData;
+
+/*
+  - Hot Topic (lots of votes)
+  - Controversial (overall 0% biased, passionate)
+  - Not Passionate (overall 0% biased, not passionate)
+  - Decided (overall 100% biased, both sides)
+  - One-Sided (overall 75% biased, one side only)
+  - You Decide (new articles/no votes)
+*/
+
+function simpleAJAX(url,callback) {
+  var req = new XMLHttpRequest();
+  req.open("GET",url);
+  req.onload = function() {
+    callback(this.responseText);
+  }
+  req.send();
+}
+
+function renderAll() {
+  var titles = ["Hot Topic","Controversial","Not Passionate","Decided","One-Sided","You Decide"];
+  for ( var i = 0; i < 6; i++ ) {
+    var element = document.getElementById("screen" + (i + 1));
+    var heading = document.createElement("h3");
+    heading.innerText = titles[i];
+    element.appendChild(heading);
+    element.appendChild(document.createElement("hr"));
+    for ( var j = 0; j < searchData[i].length; j++ ) {
+      var title = document.createElement("b");
+      title.innerText = `${searchData[i][j].title} - ${Math.abs(searchData[i][j].rating)}% Biased`;
+      var opinion = 1;
+      if ( searchData[i][j].rating > 0 ) {
+        title.className = "right";
+        opinion = 2;
+      } else if ( searchData[i][j].rating < 0 ) {
+        title.className = "left";
+        opinion = 0;
+      }
+      element.appendChild(title);
+      element.appendChild(document.createElement("br"));
+      var comment = document.createElement("textarea");
+      var selected = searchData[i][j].comments.filter(item => item.votes > 0 && item.opinion == opinion).sort((a,b) => b.votes - a.votes);
+      selected = selected[0] || null;
+      if ( selected ) {
+        selected = decodeURIComponent(selected.comment).split("\n").join(" ");
+        comment.value = `Top supporting comment: ${selected}`;
+      } else {
+        comment.value = "No supporting comment";
+      }
+      comment.rows = "4";
+      comment.disabled = "disabled";
+      element.appendChild(comment);
+      element.appendChild(document.createElement("br"));
+      element.appendChild(document.createElement("br"));
+    }
+  }
+}
+
+window.onload = function() {
+  simpleAJAX("/api/search?retr",function(data) {
+    searchData = JSON.parse(data);
+    renderAll();
+  });
+}
