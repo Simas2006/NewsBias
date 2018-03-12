0ad939b8472f06a1960b3e9ab91dabdce4ce0495
Simas2006 <sjuknelis@gmail.com>
2018-03-12 19:03:56 -0400

    Remove biased text for graphic

--- web/article/script.js
+++ web/article/script.js
@@ -168,7 +168,7 @@ function renderAll() {
   }
   var activeCount = 0;
   var interval = setInterval(function() {
-    document.getElementById("complete").innerText = `${Math.min(activeCount,Math.abs(data.votes.rating))}% Biased`;
+    document.getElementById("complete").innerText = `${Math.min(activeCount,Math.abs(data.votes.rating))}%`;
     document.getElementById("complete").className = data.votes.rating > 0 ? "right" : data.votes.rating < 0 ? "left" : "";
     document.getElementById("leftvote").innerHTML = `The left (${Math.min(activeCount,data.votes.left.sum / (data.votes.left.sum + data.votes.right.sum) * 100 || 0)}% of everyone) said:<br />${Math.min(activeCount,Math.abs(data.votes.left.rating))}% Biased<br />${data.votes.left.rating < 0 ? "&lt;" : ""}&#9473;&#9473;&#9473;&#9473;${data.votes.left.rating > 0 ? "&gt;" : ""}`
     document.getElementById("leftvote").className = data.votes.left.rating > 0 ? "right" : data.votes.left.rating < 0 ? "left" : "";
